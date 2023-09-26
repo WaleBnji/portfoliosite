5302fec1ebf9c9a128f1e660e0b23072b7c7a262
Wale Bnji <walebnji@gmail.com>
2023-09-26 22:59:31 +0100

    Latest commit

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -9,7 +9,7 @@ export default function HeroSection() {
     <section id='heroSection' className='hero--section'>
       <div className='hero--section--content--box'>
         <div className='hero--section--content'>
-          <p className='section--title'>Hey, I'm Wale</p>
+          <p className='section--title'>Hey, I'm WaleBnji</p>
           <h1 className='hero--section--title'>
             <span className='hero--section-title--color'>A Front End</span> <br />
             Developer
